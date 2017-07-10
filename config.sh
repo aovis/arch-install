82b392218d559800e11be1ebd94d752dd88d9836
aovis <bahiaps@sina.com>
2017-07-10 14:27:01 +0800

    commit

--- config.sh
+++ config.sh
@@ -238,6 +238,8 @@ then
 	chmod -w /etc/sudoers
 fi
 usermod -aG root,bin,daemon,tty,disk,games,network,video,audio $USER
+cp /etc/X11/xinit/xinitrc /root/.xinitrc
+cp /etc/X11/xinit/xinitrc /home/$USER/.xinitrc
 if [ "$VIDEO" == "4" ];
 then  
 	gpasswd -a $USER bumblebee
